07228be2a5b153f0e8f8f40655255874edc6a801
erikboren <erik.boren@gmail.com>
2021-11-04 17:32:16 +0100

    Prototypes

--- script.js
+++ script.js
@@ -11,4 +11,29 @@ function book(title, author, pages, read){
 
 const theHobbit = new book("The Hobbit", "J.R.R Tolkien", "451","yes");
 
-console.log(theHobbit.info());
+// console.log(theHobbit.info());
+
+// prototypes
+
+function Student(){
+   /*  this.sayName = function(){ (this does not work? )
+        console.log(this.name)
+    } */
+}
+
+Student.prototype.sayName = function(){
+    console.log(this.name)
+}
+
+function EighthGrader(name) {
+    this.name = name
+    this.grade = 8
+}
+
+EighthGrader.prototype = Object.create(Student.prototype)
+
+const carl = new EighthGrader("Carl")
+
+carl.sayName()
+
+console.log("Carls grade is: " + carl.grade)

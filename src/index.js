f80e6695af7bc5d136cd3ecf1d1fa06ab6c87527
erikboren <erik.boren@gmail.com>
2022-01-10 16:37:31 +0100

    Webpack assets guide

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 /*jshint esversion: 6 */
 
 import _ from 'lodash';
+import './style.css';
 
 function component(){
     const element = document.createElement('div');
 //lodash is now imported by this script 
     element.innerHTML = _.join(['Hello', 'webpack'],' ');
-
+    element.classList.add('hello');
     return element;
 }
 
